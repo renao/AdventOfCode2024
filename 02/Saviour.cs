e07050f7c904869f5eca44e840f137d18ac31635
René Siemer <me@renao.de>
2024-12-05 00:23:36 +0100

    Solve Dec 2nd - first riddle

---
+++ 02/Saviour.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode2024._02;
+
+public static class Saviour
+{
+    public static int SafeReports()
+    {
+        var reports = ReadReportsData();
+        return AnalyzeReports(reports);
+    }
+
+    private static int AnalyzeReports(int[][] reports)
+        => reports.Count(IsSafe);
+
+    private static bool IsSafe(int[] report)
+    {
+        var initialDirection = Math.Sign(report[0] - report[1]);
+        
+        for (var i = 0; i < report.Length - 1; i++)
+        {
+            var difference = report[i] - report[i + 1];
+            var levelDirection = Math.Sign(difference);
+            var diffValue = Math.Abs(difference);
+
+            if (levelDirection != initialDirection
+                || diffValue is < 1 or > 3)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private static int[][] ReadReportsData()
+    {
+        var readReports = new List<int[]>();
+        using (var sr = new StreamReader(Path.Combine("02","Reports.input")))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                var parts = line.Split(' ', StringSplitOptions.TrimEntries);
+                var reportLevels = parts.Select(int.Parse);
+                readReports.Add(reportLevels.ToArray());
+            }
+        }
+
+        return readReports.ToArray();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AdventOfCode2024._01;
+using AdventOfCode2024._02;
 
 Console.WriteLine("Advent of Code 2024");
 Console.WriteLine("");
 Console.WriteLine("======================");
-Console.WriteLine("Decembre, 1st 2024");
+Console.WriteLine("December, 1st 2024");
 Console.WriteLine("======================");
 Console.WriteLine("");
 Console.WriteLine($"The Total Distance between all the locations is: {Locateur.TotalDistance()}");
 Console.WriteLine($"The Similarity Score is: {Locateur.SimilarityScore()}");
+Console.WriteLine("");
+Console.WriteLine("======================");
+Console.WriteLine("December, 2nd 2024");
+Console.WriteLine("======================");
+Console.WriteLine("");
+Console.WriteLine($"There are {Saviour.SafeReports()} safe report files");
 Console.ReadKey();
