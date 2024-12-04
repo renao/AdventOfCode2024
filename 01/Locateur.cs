83694dc4e1e26230c0a51d10cbb6787ecb350bcf
René Siemer <me@renao.de>
2024-12-04 23:19:48 +0100

    Finish Dec 1st

---
+++ 01/Locateur.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode2024._01;
+
+public static class Locateur
+{
+    public static int TotalDistance()
+    {
+        int[] left, right;
+        ReadLocationData(out left, out right);
+
+        var totalDistance = 0;
+        for (int i = 0; i < left.Length; i++)
+        {
+            totalDistance += SingleDistance(left[i], right[i]);
+        }
+
+        return totalDistance;
+    }
+
+    public static int SimilarityScore()
+    {
+        int[] left, right;
+        ReadLocationData(out left, out right);
+
+        var similarityScore = 0;
+        
+        for (int i = 0; i < left.Length; i++)
+        {
+            similarityScore += SingleSimilarity(left[i], right);
+        }
+        
+        return similarityScore;
+    }
+
+    private static int SingleDistance(int left, int right) 
+        => Math.Abs(left - right);
+
+    private static int SingleSimilarity(int left, int[] right)
+    {
+        var count = right.Count(i => left == i);
+        return count * left;
+    }
+
+
+    private static void ReadLocationData(out int[] left, out int[] right)
+    {
+        var readLeft = new List<int>();
+        var readRight = new List<int>();
+        using (var sr = new StreamReader(Path.Combine("01","Locations.input")))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                var parts = line.Split(' ', StringSplitOptions.TrimEntries);
+                readLeft.Add(int.Parse(parts.First()));
+                readRight.Add(int.Parse(parts.Last()));
+            }
+        }
+
+        left = readLeft.Order().ToArray();
+        right = readRight.Order().ToArray();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2024._01;
+
+Console.WriteLine("Advent of Code 2024");
+Console.WriteLine("");
+Console.WriteLine("======================");
+Console.WriteLine("Decembre, 1st 2024");
+Console.WriteLine("======================");
+Console.WriteLine("");
+Console.WriteLine($"The Total Distance between all the locations is: {Locateur.TotalDistance()}");
+Console.WriteLine($"The Similarity Score is: {Locateur.SimilarityScore()}");
+Console.ReadKey();
